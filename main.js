b247f541439e87def24f044978154f02b28abd73
Eva-Liisa Edela <evaliisae@Eva-Liisas-MacBook-Air.local>
2018-10-10 16:49:14 +0300

    valmis

--- main.js
+++ main.js
@@ -1,1 +1,71 @@
-var numInput = document.getElementById('numInput');
+var numInput = document.querySelector('#numInput');
+var factOutput = document.querySelector('#factOutput');
+var incInput = document.querySelector('#incInput');
+
+const getFact = () => {
+	factOutput.innerHTML = ''
+
+
+	var number = numInput.value;
+	var inc = incInput.value;
+	var baseUrl = 'http://numbersapi.com/'
+	//vaja saada sarnane URL
+	//http://numbersapi.com/1..3
+
+	var url = `${baseUrl}${number}..${parseInt(number) + parseInt(inc)}`;
+
+	if (number != '' && inc != '') {
+		axios.get(url)
+		.then(response => {
+			for (var item in response.data){
+				var li = document.createElement('li');
+				li.innerText = response.data[item];
+				factOutput.appendChild(li)
+			}
+		})
+		.catch(error => {
+			console.log(error);
+		})
+		}	
+		
+	}
+
+	numInput.addEventListener('input', getFact);
+	incInput.addEventListener('input', getFact);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
